76806acfd4899fe9129cd401e2ec8e837cd90b35
Araceli271115 <marlenixgc07@gmail.com>
2019-03-03 17:40:21 -0600

    Se modifico la interfaz
    Se cambio en su mayoria la interfaz de la calculadora para que pudiese  acomodar de una mejor manera en el celular

---
+++ Calculadora/app/src/main/java/com/example/calculadora/Operaciones.java
@@ -0,0 +1,10 @@
+package com.example.calculadora;
+
+public class Operaciones {
+
+    public boolean validarRango(int numero, int limiteInferior, int limiteSuperior){
+        return numero >= limiteInferior && numero <= limiteSuperior;
+    }
+
+
+}
