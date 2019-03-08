4a3c0289e2469c667cb04d00fa596dfaadc979e6
Araceli271115 <marlenixgc07@gmail.com>
2019-03-07 21:22:35 -0600

    Creación de métodos para operaciones
    Realización de la mayoria de las operaciones

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -2,57 +2,176 @@ package com.example.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnCero,btnUno, btnDos, btnTres, btnCuatro, btnCinco, btnSeis, btnSiete, btnOcho,
-            btnNueve, btnDel,btnAc, btnA, btnPorcentaje, btnFactorial, btnRaiz, btnExpo,
+    int btnCero, btnUno, btnDos, btnTres, btnCuatro, btnCinco, btnSeis, btnSiete, btnOcho,
+            btnNueve, btnDel, btnAc, btnPorcentaje, btnFactorial, btnRaiz, btnExpo,
             btnSuma, btnResta, btnMultiplica, btnDivide, btnPunto, btnIgual;
-    TextView mostrarResultado;
-    TextView captura;
-    String entradaPantalla;
+
+    TextView pantallaCaptura;
+    String valorIngresado = "";
+    String valorIngresado2 = "";
+    double resultado;
+    String operador;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCero=(Button)findViewById(R.id.Cero);
-        btnUno = (Button)findViewById(R.id.Uno);
-        btnDos = (Button)findViewById(R.id.Dos);
-        btnTres = (Button)findViewById(R.id.Tres);
-        btnCuatro = (Button)findViewById(R.id.Cuatro);
-        btnCinco = (Button)findViewById(R.id.Cinco);
-        btnSeis = (Button)findViewById(R.id.Seis);
-        btnSiete = (Button)findViewById(R.id.Siete);
-        btnOcho = (Button)findViewById(R.id.Ocho);
-        btnNueve = (Button)findViewById(R.id.Nueve);
-        btnDel = (Button)findViewById(R.id.Del);
-        btnAc = (Button)findViewById(R.id.Ac);
-        btnA = (Button)findViewById(R.id.A);
-        btnPorcentaje = (Button)findViewById(R.id.Porcentaje);
-        btnFactorial = (Button)findViewById(R.id.Factorial);
-        btnRaiz = (Button)findViewById(R.id.Raiz);
-        btnExpo = (Button)findViewById(R.id.Exponente);
-        btnSuma = (Button)findViewById(R.id.Mas);
-        btnResta = (Button)findViewById(R.id.Menos);
-        btnMultiplica = (Button)findViewById(R.id.Multiplicar);
-        btnDivide = (Button)findViewById(R.id.Division);
-        btnPunto = (Button)findViewById(R.id.Punto);
-        btnIgual = (Button)findViewById(R.id.Resultado);
-        mostrarResultado = (TextView)findViewById(R.id.Res);
-        captura = (TextView)findViewById(R.id.Entrada);
+        btnDel = R.id.Del;
+        btnAc = R.id.Ac;
+        btnPorcentaje = R.id.Porcentaje;
+        btnFactorial = R.id.Factorial;
+        btnRaiz = R.id.Raiz;
+        btnExpo = R.id.Exponente;
+        btnSuma = R.id.Mas;
+        btnResta = R.id.Menos;
+        btnMultiplica = R.id.Multiplicar;
+        btnDivide = R.id.Division;
+        btnPunto = R.id.Punto;
+        btnIgual = R.id.Resultado;
+        pantallaCaptura = (TextView) findViewById(R.id.Entrada);
+    }
 
 
-     btnUno.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-             entradaPantalla = mostrarResultado.getText().toString();
-             entradaPantalla = entradaPantalla + "1";
-             captura.setText(entradaPantalla);
-         }
-     });
+    public void numeros(View view) {
+        switch (view.getId()) {
+            case R.id.Cero:
+                valorIngresado = valorIngresado + "0";
+                break;
+            case R.id.Uno:
+                valorIngresado = valorIngresado + "1";
+                break;
+            case R.id.Dos:
+                valorIngresado = valorIngresado + "2";
+                break;
+            case R.id.Tres:
+                valorIngresado = valorIngresado + "3";
+                break;
+            case R.id.Cuatro:
+                valorIngresado = valorIngresado + "4";
+                break;
+            case R.id.Cinco:
+                valorIngresado = valorIngresado + "5";
+                break;
+            case R.id.Seis:
+                valorIngresado = valorIngresado + "6";
+                break;
+            case R.id.Siete:
+                valorIngresado = valorIngresado + "7";
+                break;
+            case R.id.Ocho:
+                valorIngresado = valorIngresado + "8";
+                break;
+            case R.id.Nueve:
+                valorIngresado = valorIngresado + "9";
+                break;
+            case R.id.Punto:
+                valorIngresado = valorIngresado + ".";
+                break;
+        }
+        pantallaCaptura.setText(valorIngresado);
+    }
+
+    public void operaciones(View view) {
+        switch (view.getId()) {
+            case R.id.Mas:
+                operador = "+";
+                break;
+            case R.id.Menos:
+                operador = "-";
+                break;
+            case R.id.Multiplicar:
+                operador = "*";
+                break;
+            case R.id.Division:
+                operador = "/";
+                break;
+            case R.id.Exponente:
+                operador = "exponente";
+                break;
+            case R.id.Raiz:
+                operador = "raiz";
+                break;
+            case R.id.Factorial:
+                operador = "factorial";
+                break;
+        }
+        valorIngresado2 = valorIngresado;
+        valorIngresado = "";
+    }
+
+    public void resultado(View view) {
+
+        if (!operador.equals("") && !valorIngresado2.equals("") && !valorIngresado.equals("")) {
+            switch (operador) {
+                case "+":
+                    resultado = Double.parseDouble(valorIngresado2) + Double.parseDouble(valorIngresado);
+                    break;
+                case "-":
+                    resultado = Double.parseDouble(valorIngresado2) - Double.parseDouble(valorIngresado);
+                    break;
+                case "*":
+                    resultado = Double.parseDouble(valorIngresado2) * Double.parseDouble(valorIngresado);
+                    break;
+                case "/":
+                    resultado = Double.parseDouble(valorIngresado2) / Double.parseDouble(valorIngresado);
+                    break;
+                case "exponente":
+                    resultado = (Double) Math.pow(Double.parseDouble(valorIngresado2), Double.parseDouble(valorIngresado));
+                    break;
+                case "raiz":
+                    resultado = (Double) Math.pow(Double.parseDouble(valorIngresado), (1 / Double.parseDouble(valorIngresado2)));
+                    break;
+
+            }
+            if (operador.equals( "factorial") && valorIngresado.equals("")) { // no puede ir un numero despues de factorial
+                resultado = 1;
+                for (int i = Integer.parseInt(valorIngresado2); i > 0; i--) {
+                    resultado = resultado * i;
+                }
+            }else{
+                Toast.makeText(MainActivity.this, "Error Ingresaste un valor despues del signo factorial ", Toast.LENGTH_LONG).show();
+            }
+
+
+            pantallaCaptura.setText("" + resultado); // se imprime el resultado el la pantalla
+            valorIngresado = resultado+"";  // para que el valor se quede guardado
+
+        } else {
+            Toast.makeText(MainActivity.this, "Error ", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+
+    public void borrado(View view) {
+        valorIngresado = pantallaCaptura.getText().toString();
+        switch (view.getId()) {
+            case R.id.Ac:
+                valorIngresado = "";
+                valorIngresado2 = "";
+                operador = "";
+                resultado = 0;
+                break;
+            case R.id.Del:
+                if (!valorIngresado.equals("")) {  // marcaba error porque ya no encontraba subString
+                    if (valorIngresado.equals(resultado + "")) {    //no puede borrrar si es el resultado
+                        Toast.makeText(MainActivity.this, "Error no puedes eliminar elementos del resultado", Toast.LENGTH_LONG).show();
+                    } else {
+                        valorIngresado = valorIngresado.substring(0, valorIngresado.length() - 1);
+                        break;
+                    }
+                }
+        }
+        pantallaCaptura.setText(valorIngresado);
     }
 }
